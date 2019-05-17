3a63bf80c02f93d30c2ecd6bc6b1524636ef216d
Wagner <wagnerhsu@hotmail.com>
2019-05-17 16:05:53 +0800

    Add NUnitTest

---
+++ NUnitTest/NUnitTest/DemoTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NUnitTest
+{
+    [TestFixture]
+    public class DemoTests
+    {
+        [Test]
+        public void Success()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void Error()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
